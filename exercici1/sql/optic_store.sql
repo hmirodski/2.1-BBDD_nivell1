d5e9261317d5d604325e969b1e30394b6b897686
Horacio Miroddi <48515132+hmirodski@users.noreply.github.com>
2025-12-03 12:36:08 +0100

    feat: created glasses table

--- exercici1/sql/optic_store.sql
+++ exercici1/sql/optic_store.sql
@@ -3,19 +3,32 @@ CREATE DATABASE optics_store;
 USE optics_store;
 
 CREATE TABLE suppliers (
-nif VARCHAR(15) PRIMARY KEY UNIQUE NOT NULL,
-name VARCHAR(100) NOT NULL,
-street VARCHAR(100),
-num VARCHAR(10),
-floor VARCHAR(10),
-door VARCHAR(10),
-city VARCHAR(50),
-postal_code VARCHAR(5),
-country VARCHAR(50) DEFAULT 'Spain',
-phone VARCHAR(20),
-fax VARCHAR(20)
-)
+	nif VARCHAR(15) PRIMARY KEY UNIQUE NOT NULL,
+	name VARCHAR(100) NOT NULL,
+	street VARCHAR(100),
+	num VARCHAR(10),
+	floor VARCHAR(10),
+	door VARCHAR(10),
+	city VARCHAR(50),
+	postal_code VARCHAR(5),
+	country VARCHAR(50) DEFAULT 'Spain',
+	phone VARCHAR(20),
+	fax VARCHAR(20)
+);
 
+CREATE TABLE glasses (
+    id_glasses int AUTO_INCREMENT PRIMARY KEY,
+    brand VARCHAR(20),
+    left_lens VARCHAR(20),
+    right_lens VARCHAR(20),
+    frame_type ENUM('rimless', 'plastic', 'metal'),
+    frame_color VARCHAR(30),
+    lens_color VARCHAR(30),
+    price DECIMAL(8 , 2 ),
+    id_supplier INT,
+    FOREIGN KEY (nif)
+        REFERENCES Supplier (nif)
+);
 
 
 
