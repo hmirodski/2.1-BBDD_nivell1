43316a800e7f6f1622ba2d3ba366a992eb77fcfe
Horacio Miroddi <48515132+hmirodski@users.noreply.github.com>
2025-12-03 12:22:06 +0100

    feat: created suppliers table/entity

--- exercici1/sql/optic_store.sql
+++ exercici1/sql/optic_store.sql
@@ -1,2 +1,21 @@
 DROP DATABASE IF EXISTS optics_store;
 CREATE DATABASE optics_store;
+USE optics_store;
+
+CREATE TABLE suppliers (
+nif VARCHAR(15) PRIMARY KEY UNIQUE NOT NULL,
+name VARCHAR(100) NOT NULL,
+street VARCHAR(100),
+num VARCHAR(10),
+floor VARCHAR(10),
+door VARCHAR(10),
+city VARCHAR(50),
+postal_code VARCHAR(5),
+country VARCHAR(50) DEFAULT 'Spain',
+phone VARCHAR(20),
+fax VARCHAR(20)
+)
+
+
+
+
