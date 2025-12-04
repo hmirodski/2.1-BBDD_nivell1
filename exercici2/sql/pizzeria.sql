097f95daed72e704b41bc748a12768278d5481c6
Horacio Miroddi <48515132+hmirodski@users.noreply.github.com>
2025-12-04 08:45:13 +0100

    feat: created tables customers, categories, products

--- exercici2/sql/pizzeria.sql
+++ exercici2/sql/pizzeria.sql
@@ -14,3 +14,30 @@ CREATE TABLE cities (
     FOREIGN KEY (province_id) REFERENCES provinces(id)
 );
 
+CREATE TABLE customers (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    surname VARCHAR(100) NOT NULL,
+    address VARCHAR(200) NOT NULL,
+    postal_code VARCHAR(5) NOT NULL,
+    city_id INT NOT NULL,
+    phone VARCHAR(20) NOT NULL,
+    FOREIGN KEY (city_id) REFERENCES cities(id)
+);
+
+CREATE TABLE categories (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE products (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    image VARCHAR(255),
+    price DECIMAL(8,2) NOT NULL,
+    type ENUM('pizza', 'hamburger', 'drink') NOT NULL,
+    category_id INT,
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+
