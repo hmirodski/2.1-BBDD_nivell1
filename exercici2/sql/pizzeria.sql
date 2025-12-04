23360eb3fee508ee9dc9f1e01c252103d85b866e
Horacio Miroddi <48515132+hmirodski@users.noreply.github.com>
2025-12-04 08:34:33 +0100

    feat: created provinces and cities tables

--- exercici2/sql/pizzeria.sql
+++ exercici2/sql/pizzeria.sql
@@ -1,3 +1,16 @@
 DROP DATABASE IF EXISTS pizzeria;
-CREATE DATABASE pizzeria CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE pizzeria;
 USE pizzeria;
+
+CREATE TABLE provinces (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE cities (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    province_id INT NOT NULL,
+    FOREIGN KEY (province_id) REFERENCES provinces(id)
+);
+
